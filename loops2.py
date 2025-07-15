5801a4c9aa179467b14d0bded30610f6b48af9eb
noel knowles <coalzcoley@gmail.com>
2025-07-15 20:46:13 +0300

    for loop exercise

---
+++ loops2.py
@@ -0,0 +1,16 @@
+#for loops in action
+
+fruits = ['apple', 'banana', 'cherry']
+for fruit in fruits:
+    print(fruit)
+
+message = "Hello, world!"
+for character in message:
+    print(character)
+
+numbers = [1,5,3,9]
+total = 0
+for number in numbers:
+    print(number)
+total = 1 + 5 + 3 + 9
+print("The total sum is", total)
