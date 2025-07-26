9225ba16aff7654a7bbf9b522f26b91dd661171e
noel knowles <coalzcoley@gmail.com>
2025-07-26 11:51:33 +0300

    module for basic arithmetic operations.
    This module contains functions I used to perform simple arithmetic operations, e.g. addition, subtraction, multiplication etc.

---
+++ calculator.py
@@ -0,0 +1,16 @@
+#This is a module for basic arithmetic calculations
+
+def add(a,b):             #addition function
+    return a + b
+
+def subtract(a,b):        #subtraction function
+    return a - b
+
+def multiply(a,b):        #multiplication function
+    return a * b
+
+def divide(a,b):          #division function
+    return a / b
+
+def remain(a,b):
+    return a % b
