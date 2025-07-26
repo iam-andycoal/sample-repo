61a8c24ca98e6ed49814102b2794917785819613
noel knowles <coalzcoley@gmail.com>
2025-07-26 11:54:38 +0300

    arithmetic operations
    This script used the calculator module to perform basic arithmetic operations.

---
+++ main.py
@@ -0,0 +1,13 @@
+from calculator import *   #This will import all the functions inside calculator module
+
+result_1 = add(5,3)        #Addition function
+result_2 = subtract(5,3)   #Subtraction function
+result_3 = multiply(5,3)   #Multiplication function
+result_4 = divide(5,3)     #Division function
+result_5 = remain(5,3)     #Remainder function
+
+print(result_1)
+print(result_2)
+print(result_3)
+print(result_4)
+print(result_5)
